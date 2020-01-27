3cdec8903d1253cba70450602f7f36c4120bf227
SpartaX18 <spartax18@gmail.com>
2020-01-27 20:51:47 +0100

    standings update

--- js/tables.js
+++ js/tables.js
@@ -674,6 +674,7 @@ function contestData(){
         stage.AddRecord(Juraj,"04:47.89","00:00.00","No")
         stage.AddRecord(SpartaRemixer,"04:43.77","00:00.00","No")
         stage.AddRecord(SpartaX18,"04:46.79","00:00.00","No")
+        stage.AddRecord(datsun,"04:44.15","00:00.00","No")
         rally.stages[0] = stage;
 
         stage = new Stage("Ballagyr");
@@ -681,6 +682,7 @@ function contestData(){
         stage.AddRecord(Juraj,"07:55.81","00:00.00","No")
         stage.AddRecord(SpartaRemixer,"07:08.15","00:00.00","No")
         stage.AddRecord(SpartaX18,"07:11.87","00:00.00","No")
+        stage.AddRecord(datsun,"07:33.91","00:00.00","No")
         rally.stages[1] = stage;
 
         stage = new Stage("Curraghs");
@@ -688,6 +690,7 @@ function contestData(){
         stage.AddRecord(Juraj,"10:50.37","00:00.00","No")
         stage.AddRecord(SpartaRemixer,"09:55.11","00:00.00","No")
         stage.AddRecord(SpartaX18,"09:49.42","00:00.00","No")
+        stage.AddRecord(datsun,"09:51.20","00:00.00","No")
         rally.stages[2] = stage;
 
         stage = new Stage("Tholt-y-will");
