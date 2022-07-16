f81da503eed4abec0a11bbe89e221fa2b742e391
SpartaX18 <spartax18@gmail.com>
2022-07-16 15:06:21 +0200

    Add flag pack

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let generateEntries = document.getElementById("generateEntries");
 
     for(let i=0; i<contest.participants.length; i++) {
         let participant = contest.participants[i];
-        let flagImg = `<img src="../../resources/flag_${participant.user.country}.png" style="height: 16px; border: 1px solid #CCC;" onerror="this.src='../../resources/flag_.png'" />`;
+        let flagImg = `<img src="../../resources/flags/${participant.user.country}.png" style="height: 16px; border: 1px solid #CCC;" onerror="this.src='../../resources/flags/flag_.png'" />`;
         if (generateEntries) {
             generateEntries.innerHTML += 
             `<tr>
