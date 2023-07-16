11926c417ccf062d725d5229d718ed71f493ffc0
EphemeralXXV <ephemeralxxv@gmail.com>
2023-07-16 21:26:25 +0200

    Add all necessary fields

--- js/MFMI23/resultSubmission.js
+++ js/MFMI23/resultSubmission.js
@@ -113,25 +113,89 @@ function checkCurrentLeg(schedule) {
 }
 
 function generateFormContent(form, currentLeg) {
-    const field_driver = document.createElement("div");
-    const label_driver = document.createElement("label");
-    const input_driver = document.createElement("input");
+    const field_driver = generateFormField("Driver", "driver", "driver", "text");
 
-    field_driver.setAttribute("id", "field_driver");
-    label_driver.setAttribute("id", "label_driver");
-    input_driver.setAttribute("id", "input_driver");
-
-    field_driver.appendChild(label_driver);
-    field_driver.appendChild(input_driver);
-    
     form.appendChild(field_driver);
 
+    // Add form section for each stage
     currentLeg.stages.forEach(stage => {
-        // Add form section for each stage
         const fieldset_stage = document.createElement("fieldset");
-        fieldset_stage.setAttribute("id", "fieldset_" + stage);
-        fieldset_stage.setAttribute("class", "fieldset_stage");
-        form.appendChild(stageSection);
+        const legend_stage = document.createElement("legend");
+        const field_time = document.createElement("div");
+        const field_time_min = generateFormField("MM", "timeMin_" + stage, "timeMin", "number");
+        const span_timeSeparator_minSec = document.createElement("span");
+        const field_time_sec = generateFormField("SS", "timeSec_" + stage, "timeSec", "number");
+        const span_timeSeparator_secCS = document.createElement("span");
+        const field_time_cs = generateFormField("CS", "timeCS_"  + stage, "timeCS", "number");
+        const field_twitchLink = generateFormField("Twitch Link", "twitchLink_" + stage, "twitchLink", "text");
+        const field_replayFile = generateFormField("Replay File", "replayFile_" + stage, "replayFile", "file");
+        const field_youtubeLink = generateFormField("Youtube Link", "ytLink_" + stage, "ytLink", "text");
+        const field_timeImage = generateFormField("Time Image", "timeImage_" + stage, "timeImage", "file");
+
+        setAttributes(field_time_min.getElementsByTagName("input")[0], {"min": "0", "max": "99", value: "00", "oninput": "addLeadingZero(this)"});
+        setAttributes(span_timeSeparator_minSec, {"id": "span_timeSeparator_minSec_" + stage, "class": "span_timeSeparator_minSec"});
+        setAttributes(field_time_sec.getElementsByTagName("input")[0], {"min": "0", "max": "59", value: "00", "oninput": "addLeadingZero(this)"});
+        setAttributes(span_timeSeparator_secCS, {"id": "span_timeSeparator_secCS_" + stage, "class": "span_timeSeparator_secCS"});
+        setAttributes(field_time_cs.getElementsByTagName("input")[0], {"min": "0", "max": "99", value: "00", "oninput": "addLeadingZero(this)"});
+        setAttributes(fieldset_stage, {"id": "fieldset_" + stage, "class": "fieldset_stage"});
+        setAttributes(legend_stage, {"id": "legend_" + stage, "class": "legend_stage"});
+        
+        legend_stage.textContent = stage;
+        span_timeSeparator_minSec.textContent = ":";
+        span_timeSeparator_secCS.textContent = ".";
+
+        field_time.appendChild(field_time_min);
+        field_time.appendChild(span_timeSeparator_minSec);
+        field_time.appendChild(field_time_sec);
+        field_time.appendChild(span_timeSeparator_secCS);
+        field_time.appendChild(field_time_cs);
+        fieldset_stage.appendChild(field_time);
+        fieldset_stage.appendChild(field_twitchLink);
+        fieldset_stage.appendChild(field_replayFile);
+        fieldset_stage.appendChild(field_youtubeLink);
+        fieldset_stage.appendChild(field_timeImage);
+        form.appendChild(legend_stage);
+        form.appendChild(fieldset_stage);
     });
+
+    const button_submit = document.createElement("button");
+    setAttributes(button_submit, {"id": "submitResult", "class": "submitBtn navspan", "type": "submit"});
+    button_submit.textContent = "Submit";
+    form.appendChild(button_submit);
 }
+
+function generateFormField(labelText, domId, domClass, inputType) {
+    const field = document.createElement("div");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+
+    setAttributes(field, {"id": "field_" + domId, "class": "field_" + domClass});
+    setAttributes(label, {"id": "label_" + domId, "class": "label_" + domClass, "for": "input_" + domId});
+    setAttributes(input, {"id": "input_" + domId, "class": "input_" + domClass, "type": inputType});
+
+    label.textContent = labelText;
+
+    field.appendChild(label);
+    field.appendChild(input);
+
+    return field;
+}
+
+// Set multiple attributes of an element by passing an object of attributes
+function setAttributes(element, attributes) {
+    for(var key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function addLeadingZero(input) {
+    if(input.value >= 0 && input.value < 10) {
+        input.value = "0" + input.value.toString();
+    }
+    // Prevent more than 2 digits (manual input)
+    if(input.value.toString().length > 2) {
+        input.value = input.value.toString().substr(-2, 2);
+    }
+}
+
 formSetup();
