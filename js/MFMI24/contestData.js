2b11157fbe15f371ff2f3a1a9d343279308a9b66
SpartaX18 <spartax18@gmail.com>
2024-07-26 18:16:02 +0200

    Update the contest schedule

--- js/MFMI24/contestData.js
+++ js/MFMI24/contestData.js
@@ -62,79 +62,79 @@ function contestData(){
     
     const schedule = [
         {
-            date: new Date("2023-08-07T20:00:00Z"),
+            date: new Date("2024-08-14T18:00:00Z"),
             stages: ["Clocaenog Mid", "Penmachno South"]
         },
         {
-            date: new Date("2023-08-08T20:00:00Z"),
+            date: new Date("2024-08-15T18:00:00Z"),
             stages: ["Myherin", "Hafren"]
         },
         {
-            date: new Date("2023-08-09T20:00:00Z"),
+            date: new Date("2024-08-16T18:00:00Z"),
             stages: ["Dyfi", "Gartheiniog"]
         },
         {
-            date: new Date("2023-08-10T20:00:00Z"),
+            date: new Date("2024-08-17T18:00:00Z"),
             stages: ["Chirdonhead"]
         },
         {
-            date: new Date("2023-08-11T20:00:00Z"),
+            date: new Date("2024-08-18T18:00:00Z"),
             stages: ["Falstone"]
         },
         {
-            date: new Date("2023-08-12T20:00:00Z"),
+            date: new Date("2024-08-19T18:00:00Z"),
             stages: ["Kershope"]
         },
         {
-            date: new Date("2023-08-13T20:00:00Z"),
+            date: new Date("2024-08-20T18:00:00Z"),
             stages: ["Pundershaw"]
         },
         {
-            date: new Date("2023-08-14T20:00:00Z"),
+            date: new Date("2024-08-21T18:00:00Z"),
             stages: ["Riccarton", "Newcastleton"]
         },
         {
-            date: new Date("2023-08-15T20:00:00Z"),
+            date: new Date("2024-08-22T18:00:00Z"),
             stages: ["Twiglees", "Yair", "Cardrona"]
         },
         {
-            date: new Date("2023-08-16T20:00:00Z"),
+            date: new Date("2024-08-23T18:00:00Z"),
             stages: ["Black Loch", "Glentrool"]
         },
         {
-            date: new Date("2023-08-17T20:00:00Z"),
+            date: new Date("2024-08-24T18:00:00Z"),
             stages: ["Ae"]
         },
         {
-            date: new Date("2023-08-18T20:00:00Z"),
+            date: new Date("2024-08-25T18:00:00Z"),
             stages: ["Moon and Star", "Bothwell", "Whitchester"]
         },
         {
-            date: new Date("2023-08-19T20:00:00Z"),
+            date: new Date("2024-08-26T18:00:00Z"),
             stages: ["Eccles", "Langton", "Fogo"]
         },
         {
-            date: new Date("2023-08-20T20:00:00Z"),
+            date: new Date("2024-08-27T18:00:00Z"),
             stages: ["Hamilton's Folly", "Tyrones Ditches"]
         },
         {
-            date: new Date("2023-08-21T20:00:00Z"),
+            date: new Date("2024-08-28T18:00:00Z"),
             stages: ["Feeney", "Parkanaur"]
         },
         {
-            date: new Date("2023-08-22T20:00:00Z"),
+            date: new Date("2024-08-29T18:00:00Z"),
             stages: ["Lisnamuck", "Tardree"]
         },
         {
-            date: new Date("2023-08-23T20:00:00Z"),
+            date: new Date("2024-08-30T18:00:00Z"),
             stages: ["Port Soderick", "Ballagyr", "Curraghs"]
         },
         {
-            date: new Date("2023-08-24T20:00:00Z"),
+            date: new Date("2024-08-31T18:00:00Z"),
             stages: ["Tholt-y-Will", "Injerbreck", "Cringle"]
         },
         {
-            date: new Date("2023-08-29T20:00:00Z"),
+            date: new Date("2024-09-01T18:00:00Z"),
             stages: []
         }
     ];
