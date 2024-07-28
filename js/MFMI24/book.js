db6765ca68c7f2572222de5a57242aac82575295
SpartaX18 <spartax18@gmail.com>
2024-07-28 18:13:36 +0200

    Fix book animation and update participants data.

--- js/MFMI24/book.js
+++ js/MFMI24/book.js
@@ -1,25 +1,14 @@
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
 const book = document.querySelector('#book');
+const papers = Array.from(document.querySelectorAll('.paper'));
 
-const paper1 = document.querySelector('#p1');
-const paper2 = document.querySelector('#p2');
-const paper3 = document.querySelector('#p3');
-const paper4 = document.querySelector('#p4');
-const paper5 = document.querySelector('#p5');
-const paper6 = document.querySelector('#p6');
-const paper7 = document.querySelector('#p7');
-const paper8 = document.querySelector('#p8');
-const paper9 = document.querySelector('#p9');
-const paper10 = document.querySelector('#p10');
-const paper11 = document.querySelector('#p11');
+const numOfPapers = papers.length;
+const maxLocation = numOfPapers + 1;
+let currentLocation = 1;
 
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
-
-let currentLocation = 1;
-let numOfPapers = 11;
-let maxLocation = numOfPapers + 1;
 
 function openBook() {
     book.style.transform = "translateX(50%)";
@@ -38,57 +27,28 @@ function closeBook(isAtBeggining) {
     nextBtn.style.transform = "translateX(0px)";
 }
 
+function updatePaper(paper, flipped) {
+    if(flipped) {
+        paper.classList.add("flipped");
+    }
+    else {
+        paper.classList.remove("flipped");
+    }
+    // Assign the highest z-index to the flipped page and decrement the z-index of all the remaining pages
+    // This ensures the flipped page has a higher z-index than the page it covers, but lower than the page it reveals,
+    // so the revealed page does not "pop through" the flipped page
+    papers.forEach(paper => paper.style.zIndex = parseInt(window.getComputedStyle(paper).zIndex) - 1);
+    paper.style.zIndex = numOfPapers;
+}
+
 function goNextPage() {
     if(currentLocation < maxLocation) {
-        switch(currentLocation) {
-            case 1:
-                openBook();
-                paper1.classList.add("flipped");
-                paper1.style.zIndex = 1;
-                break;
-            case 2:
-                paper2.classList.add("flipped");
-                paper2.style.zIndex = 2;
-                break;
-            case 3:
-                paper3.classList.add("flipped");
-                paper3.style.zIndex = 3;
-                break;
-            case 4:
-                paper4.classList.add("flipped");
-                paper4.style.zIndex = 4;
-                break;
-            case 5:
-                paper5.classList.add("flipped");
-                paper5.style.zIndex = 5;
-                break;
-            case 6:
-                paper6.classList.add("flipped");
-                paper6.style.zIndex = 6;
-                break;
-            case 7:
-                paper7.classList.add("flipped");
-                paper7.style.zIndex = 7;
-                break;
-            case 8:
-                paper8.classList.add("flipped");
-                paper8.style.zIndex = 8;
-                break;
-            case 9:
-                paper9.classList.add("flipped");
-                paper9.style.zIndex = 9;
-                break;
-            case 10:
-                paper10.classList.add("flipped");
-                paper10.style.zIndex = 10;
-                break;
-            case 11:
-                paper11.classList.add("flipped");
-                paper11.style.zIndex = 11;
-                closeBook(false);
-                break;
-            default:
-                throw new Error("unknown state");
+        if(currentLocation === 1) {
+            openBook();
+        }
+        updatePaper(papers[currentLocation - 1], true);
+        if(currentLocation === numOfPapers) {
+            closeBook(false);
         }
         currentLocation++;
     }
@@ -96,55 +56,12 @@ function goNextPage() {
 
 function goPrevPage() {
     if(currentLocation > 1) {
-        switch(currentLocation) {
-            case 2:
-                closeBook(true);
-                paper1.classList.remove("flipped");
-                paper1.style.zIndex = 11;
-                break;
-            case 3:
-                paper2.classList.remove("flipped");
-                paper2.style.zIndex = 10;
-                break;
-            case 4:
-                paper3.classList.remove("flipped");
-                paper3.style.zIndex = 9;
-                break;
-            case 5:
-                paper4.classList.remove("flipped");
-                paper4.style.zIndex = 8;
-                break;
-            case 6:
-                paper5.classList.remove("flipped");
-                paper5.style.zIndex = 7;
-                break;
-            case 7:
-                paper6.classList.remove("flipped");
-                paper6.style.zIndex = 6;
-                break;
-            case 8:
-                paper7.classList.remove("flipped");
-                paper7.style.zIndex = 5;
-                break;
-            case 9:
-                paper8.classList.remove("flipped");
-                paper8.style.zIndex = 4;
-                break;
-            case 10:
-                paper9.classList.remove("flipped");
-                paper9.style.zIndex = 3;
-                break;
-            case 11:
-                paper10.classList.remove("flipped");
-                paper10.style.zIndex = 2;
-                break;
-            case 12:
-                openBook();
-                paper11.classList.remove("flipped");
-                paper11.style.zIndex = 1;
-                break;
-            default:
-                throw new Error("unknown state");
+        if(currentLocation === 2) {
+            closeBook(true);
+        }
+        updatePaper(papers[currentLocation - 2], false);
+        if(currentLocation === numOfPapers + 1) {
+            openBook();
         }
         currentLocation--;
     }
--- js/MFMI24/contestData.js
+++ js/MFMI24/contestData.js
@@ -21,7 +21,7 @@ function contestData(){
     let Lewsys =            new Participant(14, website.GetUserByName('Lewsys'),            participantColor(), 'Peugeot 206 WRC');
     let sBinnala =          new Participant(16, website.GetUserByName('sBinnala'),          participantColor(), 'Seat Cordoba WRC');
     let datsun100aGTR =     new Participant(90, website.GetUserByName('SForman135LS'),      participantColor(), 'Subaru Impreza WRC');
-    let Woeringen1288 =     new Participant(7,  website.GetUserByName('Woeringen1288'),     participantColor(), 'Mitsubishi Lancer Evo V');
+    let Woeringen1288 =     new Participant(12, website.GetUserByName('Woeringen1288'),     participantColor(), 'Mitsubishi Lancer Evo V');
     let Pendzior =          new Participant(8,  website.GetUserByName('Pendzior'),          participantColor(), 'Seat Cordoba WRC');
     let KarelPipa =         new Participant(55, website.GetUserByName('Karel Pipa'),        participantColor(), 'Seat Cordoba WRC');
     let TheKetrab =         new Participant(10, website.GetUserByName('TheKetrab'),         participantColor(), 'Mitsubishi Lancer Evo V');
@@ -29,7 +29,7 @@ function contestData(){
     let Red_T =             new Participant(19, website.GetUserByName('Red_T'),             participantColor(), 'Subaru Impreza WRC');
     let PKlima =            new Participant(77, website.GetUserByName('P. Klima'),          participantColor(), 'Mitsubishi Lancer Evo V');
     let Noni =              new Participant(69, website.GetUserByName('Noni'),              participantColor(), 'Peugeot 206 WRC');
-    let InfamousGhost07 =   new Participant(70, website.GetUserByName('InfamousGhost07'),   participantColor(), 'Peugeot 206 WRC');
+    let InfamousGhost07 =   new Participant(7,  website.GetUserByName('InfamousGhost07'),   participantColor(), 'Mitusbishi Lancer Evo IV');
     let BrosTheThird =      new Participant(30, website.GetUserByName('BrosTheThird'),      participantColor(), 'Subaru Impreza WRC');
     contest.participants.push(
         Erwto,
