a97a44592f53df798bcd1cf8283b78e564f457d8
SpartaX18 <spartax18@gmail.com>
2024-08-07 19:58:21 +0200

    Update Noni's number, update driver profiles.

--- js/MFMI24/contestData.js
+++ js/MFMI24/contestData.js
@@ -28,7 +28,7 @@ function contestData(){
     let Tribell =           new Participant(39, website.GetUserByName('Tribell'),           participantColor(), 'Mitusbishi Lancer Evo IV');
     let Red_T =             new Participant(19, website.GetUserByName('Red_T'),             participantColor(), 'Subaru Impreza WRC');
     let PKlima =            new Participant(77, website.GetUserByName('P. Klima'),          participantColor(), 'Mitsubishi Lancer Evo V');
-    let Noni =              new Participant(69, website.GetUserByName('Noni'),              participantColor(), 'Peugeot 206 WRC');
+    let Noni =              new Participant(44, website.GetUserByName('Noni'),              participantColor(), 'Peugeot 206 WRC');
     let InfamousGhost07 =   new Participant(7,  website.GetUserByName('InfamousGhost07'),   participantColor(), 'Mitusbishi Lancer Evo IV');
     let BrosTheThird =      new Participant(30, website.GetUserByName('BrosTheThird'),      participantColor(), 'Subaru Impreza WRC');
     contest.participants.push(
